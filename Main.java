55220562f895ab6292f3360ede7ada23de248c6c
fidusenpai <36771187+fidusenpai@users.noreply.github.com>
2018-03-06 17:34:50 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
-
+/* NAMES
+03/08/2018
+This is the start (main) of the game: Hook */
 public class Main {
-
-	
 	public static void main(String[] args) {
-		
 		new MainMenu();
-
-
+		//see Main Menu class
 	}
-
 }
