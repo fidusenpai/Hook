dbce7a98b737edcee7dab61e88ded099d7bf43c6
fidusenpai <36771187+fidusenpai@users.noreply.github.com>
2018-03-06 18:09:42 -0500

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -14,37 +14,45 @@ public class MainMenu extends Level {
 	private Label title = new Label("Hook");
 	private Font menuFont = new Font("Courier", Font.BOLD, 40);
 	private Font titleFont = new Font("Courier", Font.BOLD, 100);
-		/*Generates the Main Menu
+		/*Generates the components for the Main Menu
 		which is two buttons: One to allow the player to play and the other to allower the player to exit the program 
 		and a label that outputs the title of the game: Hook. */
 	
 	MainMenu(){
 		super("Hook: Main Menu");
-			
+			//The main menu inherits the frame's properties for which the program will be on (see Level super class)
 		title.setBounds(275, 100, 600, 100);
 		title.setFont(titleFont);
 		add(title);
-		
+			//Set the properties of the label and add it to the frame
+	
 		Exit.setBounds(100,600,600,50);
 		Exit.setFont(menuFont);
+			//Set the properties of the exit button
 		Exit.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
+					//When the exit button is clicked, close the program
 			}
 		});
+			
 		add(Exit);
+			//Add the exit button to the frame
 		
 		StartGame.setBounds(100, 500, 600, 50);
 		StartGame.setFont(menuFont);
+			//Set the properties of the start button
 		StartGame.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				new LevelOne();
 				dispose();
+					//When the start button is clicked, level one will generate and appear. Main menu's frame is disposed
 			}
 		});
 		add(StartGame);
+			 //Add the start button to the frame
 	}
 	
 }
